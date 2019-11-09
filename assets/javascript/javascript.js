fb18bea38eb77ecbef424e7b6cb9855b3f064aeb
Alex Santillan <asantill68@yahoo.com>
2019-11-09 13:08:17 -0600

    LOST

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,7 +2,19 @@
 // Get API KEY for ticketmaster and google maps
 // sift through JSON data to retreive objects to attach
 // build url to query the API
-// Get AJAX call for maps to get geolocation 
+// Get AJAX call for maps to get geolocation
+$('button').on('click', function(e){
+	e.preventDefault();
+
+	queryURL = "http://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=AIzaSyDHluTmVleXvATJosAhYBe1mE-rCmqGru0";
+		console.log(queryURL);
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).done(function(response){
+		console.log(response);
+		 });
+}); 
 // Get AJAX call for ticketmaster to get events and locations
 // Using Jquery create an onclick event listener (to retrieve user inputs)
 // create flow to manipulate data from JSON
